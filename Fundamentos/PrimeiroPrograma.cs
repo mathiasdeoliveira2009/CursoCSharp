202523e1e99594a47a50c271f8f346078f902b29
mathiasdeoliveira2009 <mathiasdeoliveira2009@gmail.com>
2026-06-01 14:47:27 -0300

    Inicializando Projeto!

---
+++ Fundamentos/PrimeiroPrograma.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Fundamentos {
+    internal class PrimeiroPrograma {
+        public static void Executar(string[] args) {
+            Console.WriteLine("Primeiro Programa!");
+            Console.WriteLine("Sexta não tem aula!!");
+        }
+    }
+}
